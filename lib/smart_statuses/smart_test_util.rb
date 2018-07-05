1e84b3a1ac8855213fac4853180e75942536de8f
sukhbir-singh <sukhbir947@gmail.com>
2018-07-05 07:17:23 +0530

    updated smart_util_lib file

--- lib/smart_statuses/smart_test_util.rb
+++ lib/smart_statuses/smart_test_util.rb
@@ -4,18 +4,19 @@ class SmartTestUtil
 			@disks = DiskUtils.stats
 			response = []
 			@disks.each_with_index do |disk, index|
-				status = read_smart_status(create_file_and_get_path(disk[:device]))
-				color = (status=="PASSED")? "green": "red";
+				status = read_smart_status(get_created_file_path(disk[:device]))
+				next if status.blank?
+				color = get_color(status)
 				response << { model: disk[:model], device: disk[:device], 
 					health_check: status, color: color }
 			end
 			return response
 		end
 
-		def create_file_and_get_path(device)
-			# create the status file and return file path
-			# need to implement for production
-			get_sample_file_path(rand(2))
+		def get_created_file_path(device)
+			return nil if device.blank?
+			status_file = Rails.root+Dir["plugins/*smart_statuses/db/smartctl-"+device[5..-1]][0]
+			read_smart_status(status_file)
 		end
 
 		def sample_stats
@@ -23,7 +24,7 @@ class SmartTestUtil
 			response = []
 			@disks.each_with_index do |disk, index|
 				status = read_smart_status(get_sample_file_path(index))
-				color = (status=="PASSED")? "green": "red";
+				color = get_color(status)
 				response << { model: disk[:model], device: disk[:device], 
 					health_check: status, color: color }
 			end
@@ -31,16 +32,33 @@ class SmartTestUtil
 		end
 
 		def get_sample_file_path(index)
-			if index.blank? or index%2 == 0
-				Rails.root+Dir["plugins/*smart_statuses/db/sample-data/smartctl-output-sample1.txt"][0]
+			if index.blank?
+				Rails.root+Dir["plugins/*smart_statuses/db/sample-data/smartctl-output-sample"+(1+rand(3)).to_s+".txt"][0]
 			else
-				Rails.root+Dir["plugins/*smart_statuses/db/sample-data/smartctl-output-sample2.txt"][0]
+				Rails.root+Dir["plugins/*smart_statuses/db/sample-data/smartctl-output-sample"+(1+index%3).to_s+".txt"][0]
+			end
+		end
+
+		def get_color(status)
+			if status =~ /PASSED/
+				"green"
+			elsif status =~ /FAILED/
+				"red"
+			else
+				"blue"
 			end
 		end
 
 		def read_smart_status(filepath)			
-			return nil unless File.exists?(filepath)
-			IO.readlines(filepath)[3][50..-1]
+			return "Some error occurred" unless File.exists?(filepath)
+			file = File.new(filepath)
+			line_num = file.find_index { |line| line =~ /START OF READ SMART DATA SECTION/ }
+
+			if line_num.blank?
+				return "Device Lacks SMART Compatibility."
+			else
+				return IO.readlines(filepath)[line_num + 1][50..-1]
+			end
 		end
 	end
 end
